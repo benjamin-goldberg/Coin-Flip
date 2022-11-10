88c11e048330e653fe2cb6459a3c6a80b5f89efa
Benjamin Goldberg <52329317+benjamin-goldberg@users.noreply.github.com>
2022-11-10 15:03:13 -0500

    Add withdraw / add random inputs / rename items

--- coinFlip.sol
+++ coinFlip.sol
@@ -13,34 +13,38 @@ contract CoinFlipper is Ownable {
     uint StartingGame = 0;
     uint randomNonce = 0;
 
-    struct BaseGame {
+    struct Game {
         address Player1;
         uint GameNum;
         uint Stake;
-        uint Status;
+        bool Filled;
     }
 
-    BaseGame[] public Game;
+    Game[] public Games;
+
+    function withdraw() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
 
     function NewGame(uint _stake) external payable {
         require(msg.value == _stake);
         require(msg.value >= 0.0001 ether);
         StartingGame = StartingGame.add(1);
-        Game.push(BaseGame(msg.sender, StartingGame, msg.value, 0));
+        Games.push(Game(msg.sender, StartingGame, msg.value, false));
     }
 
     function FillGame(uint _GameNum) external payable {
-        require(msg.value == Game[_GameNum].Stake);
-        require(Game[_GameNum].Status == 0);
+        require(msg.value == Games[_GameNum].Stake);
+        require(Games[_GameNum].Filled == false);
         randomNonce.add(1);
-        uint random = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randomNonce, _GameNum))) % 100;
+        uint random = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randomNonce, _GameNum, Games[_GameNum].Player1))).mod(100);
         if (random >= 50) {
-            payable(address(msg.sender)).transfer(msg.value.mul(2));
+            payable(address(msg.sender)).transfer(msg.value.mul(2).mul(98).div(100));
         } else {
             
-            payable(address(Game[_GameNum].Player1)).transfer(msg.value.mul(2));
+            payable(address(Games[_GameNum].Player1)).transfer(msg.value.mul(2).mul(98).div(100));
         }
-        Game[_GameNum].Status = 1;
+        Games[_GameNum].Filled = true;
     }
 
 } 
