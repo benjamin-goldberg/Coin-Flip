0536fc82562cdb3c6866ca3efa27ae7d0a28b67c
Benjamin Goldberg <52329317+benjamin-goldberg@users.noreply.github.com>
2022-11-12 13:04:53 -0500

    I think I finally got the oracle to work

--- CoinFlip.sol
+++ CoinFlip.sol
@@ -5,11 +5,59 @@ pragma solidity ^0.8.13;
 
 import "./Ownable.sol";
 import "./SafeMath.sol";
-import "./RandomOracle.sol";
+import './VRFCoordinatorV2Interface.sol';
+import './VRFConsumerBaseV2.sol';
+import './ConfirmedOwner.sol';
 
-contract CoinFlip is Ownable, RandomOracle {
+contract CoinFlip is Ownable, VRFConsumerBaseV2, ConfirmedOwner {
+
+    struct RequestStatus {
+        bool fulfilled; // whether the request has been successfully fulfilled
+        bool exists; // whether a requestId exists
+        uint256[] randomWords;
+    }
+    mapping(uint256 => RequestStatus) public s_requests; /* requestId --> requestStatus */
+    VRFCoordinatorV2Interface COORDINATOR;
+
+    uint64 s_subscriptionId;
+    bytes32 keyHash = 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
+    uint32 callbackGasLimit = 100000;
+    uint16 requestConfirmations = 3;
+    uint32 numWords = 1;
+
+    constructor(uint64 subscriptionId)
+        VRFConsumerBaseV2(0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D)
+        ConfirmedOwner(msg.sender)
+    {
+        COORDINATOR = VRFCoordinatorV2Interface(0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D);
+        s_subscriptionId = subscriptionId;
+    }
+
+    function _requestRandomWords(uint _gameNum) internal returns (uint256 requestId) {
+        requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+        s_requests[requestId] = RequestStatus({randomWords: new uint256[](0), exists: true, fulfilled: false});
+        Games[_gameNum].randomId = requestId;
+        Games[_gameNum].randomIdExists = true;
+        return requestId;
+    }
+
+    function fulfillRandomWords(uint256 _requestId, uint256[] memory _randomWords) internal override {
+        require(s_requests[_requestId].exists, 'request not found');
+        s_requests[_requestId].fulfilled = true;
+        s_requests[_requestId].randomWords = _randomWords;
+        Games[_requestId].randomNum = _randomWords;
+        Games[_requestId].randomFulfilled = true;
+    }
 
     using SafeMath for uint;
+
+    uint startingGame = 0;
 
     struct Game {
         uint gameNum;
@@ -22,7 +70,7 @@ contract CoinFlip is Ownable, RandomOracle {
         address winner;
         uint amountWon;
         string winningSide;
-        uint256[] randomId;
+        uint256 randomId;
         bool randomFulfilled;
         bool randomIdExists;
         uint256[] randomNum;
@@ -39,7 +87,7 @@ contract CoinFlip is Ownable, RandomOracle {
         require(msg.value >= 0.00001 ether, "Too small of an amount.");
         require(uint(keccak256(abi.encodePacked(_HeadsOrTails))) == uint(keccak256(abi.encodePacked("Heads"))) || uint(keccak256(abi.encodePacked("Tails"))) == uint(keccak256(abi.encodePacked(_HeadsOrTails))), "You must pick Heads or Tails.");
         startingGame = startingGame.add(1);
-        Games.push(Game(startingGame, msg.sender, _HeadsOrTails, msg.value, 0x0000000000000000000000000000000000000000, "", false, 0x0000000000000000000000000000000000000000, 0, "", new uint256[](0), false, false, new uint256[](0), 0));
+        Games.push(Game(startingGame, msg.sender, _HeadsOrTails, msg.value, 0x0000000000000000000000000000000000000000, "", false, 0x0000000000000000000000000000000000000000, 0, "", 0, false, false, new uint256[](0), 0));
     }
 
     function fillGame(uint _gameNum) public payable {
@@ -52,15 +100,9 @@ contract CoinFlip is Ownable, RandomOracle {
             Games[_gameNum].player2Side = "Heads";
         }
         Games[_gameNum].filled = true;
-        Games[_gameNum].randomId = COORDINATOR.requestRandomWords(
-            keyHash,
-            s_subscriptionId,
-            requestConfirmations,
-            callbackGasLimit,
-            numWords
-        );
+        _requestRandomWords(_gameNum);
     }
-    
+
     function flipGame(uint _gameNum) external {
         require(Games[_gameNum].randomFulfilled == true);
         Games[_gameNum].roll = Games[_gameNum].randomNum[0].mod(100);
@@ -69,12 +111,12 @@ contract CoinFlip is Ownable, RandomOracle {
         } else {
             Games[_gameNum].winningSide = "Heads";
         }
-        if (Games[_gameNum].winningSide = Games[_gameNum].player1Side) {
+        if ((uint(keccak256(abi.encodePacked(Games[_gameNum].winningSide))) == (uint(keccak256(abi.encodePacked(Games[_gameNum].player1Side)))))) {
             payable(Games[_gameNum].player1).transfer(msg.value.mul(2).mul(98).div(100));
-            Games[_gameNum].winner = msg.sender;
+            Games[_gameNum].winner = Games[_gameNum].player2;
         } else {
-            payable(Games[_gameNum].player1).transfer(msg.value.mul(2).mul(98).div(100));
-            Games[_gameNum].winner = Games[_gameNum].player1;
+            payable(Games[_gameNum].player2).transfer(msg.value.mul(2).mul(98).div(100));
+            Games[_gameNum].winner = Games[_gameNum].player2;
         }
         
         Games[_gameNum].amountWon = msg.value.mul(2).mul(98).div(100);
