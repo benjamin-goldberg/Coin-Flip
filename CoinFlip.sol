cb7e0407116ba2b67633185abb06784dc0ec483e
Benjamin Goldberg <52329317+benjamin-goldberg@users.noreply.github.com>
2022-11-12 14:15:07 -0500

    Undoing that undo

--- CoinFlip.sol
+++ CoinFlip.sol
@@ -15,7 +15,7 @@ contract CoinFlip is Ownable, VRFConsumerBaseV2, ConfirmedOwner {
 
     uint64 s_subscriptionId;
     bytes32 keyHash = 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
-    uint32 callbackGasLimit = 500000;
+    uint32 callbackGasLimit = 300000;
     uint16 requestConfirmations = 3;
     uint32 numWords = 1;
 
@@ -25,6 +25,10 @@ contract CoinFlip is Ownable, VRFConsumerBaseV2, ConfirmedOwner {
     {
         COORDINATOR = VRFCoordinatorV2Interface(0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D);
         s_subscriptionId = subscriptionId;
+    }
+
+    function changeGasLimit(uint32 _newGasLimit) external onlyContractOwner {
+        callbackGasLimit = _newGasLimit;
     }
 
     function _requestRandomWords(uint _gameNum) internal returns (uint256 requestId) {
