4c1e0c018aee4d64da94fe734f004162994dfeb8
Benjamin Goldberg <52329317+benjamin-goldberg@users.noreply.github.com>
2022-11-12 23:56:30 -0500

    Cleaning it all up

--- CoinFlip.sol
+++ CoinFlip.sol
@@ -2,43 +2,52 @@
 
 pragma solidity ^0.8.13;
 
-
-import "./Ownable.sol";
 import "./SafeMath.sol";
 import './VRFCoordinatorV2Interface.sol';
 import './VRFConsumerBaseV2.sol';
 import './ConfirmedOwner.sol';
 
-contract CoinFlip is Ownable, VRFConsumerBaseV2, ConfirmedOwner {
+contract CoinFlip is VRFConsumerBaseV2, ConfirmedOwner {
 
-struct RequestStatus {
-        bool fulfilled; // whether the request has been successfully fulfilled
-        bool exists; // whether a requestId exists
-        uint256[] randomWords;
-        uint gameNum;
+    using SafeMath for uint;
+    using SafeMath for uint64;
+    using SafeMath for uint32;
+    using SafeMath for uint16;
+    using SafeMath for uint8;
+
+    struct Game {
+        uint32 gameNum;
+        address player1;
+        string player1Side;
+        uint256 stake;
+        address player2;
+        string player2Side;
+        bool filled;
+        address winner;
+        string winningSide;
+        uint256 randomId;
+        bool randomIdExists;
+        uint256[] randomNum;
+        uint256 roll;
     }
-    mapping(uint256 => RequestStatus) public s_requests;
+    Game[] public Games;
 
-
-
-
-/*
     struct RequestStatus {
         bool fulfilled;
         bool exists;
-        uint256[] randomNum;
-        uint gameNum;
-        uint requestId;
+        uint256[] randomWords;
+        uint32 gameNum;
     }
-    RequestStatus[] public Requests;
-  */  
+    mapping(uint256 => RequestStatus) public s_requests;
+
     VRFCoordinatorV2Interface COORDINATOR;
 
+    uint32 startingGame = 0;
     uint64 s_subscriptionId;
     bytes32 keyHash = 0x354d2f95da55398f44b7cff77da56283d9c6c829a4bdf1bbcaf2ad6a4d081f61;
     uint32 callbackGasLimit = 2500000;
-    uint16 requestConfirmations = 3;
     uint32 numWords = 1;
+    uint16 requestConfirmations = 3;
 
     constructor(uint64 subscriptionId)
         VRFConsumerBaseV2(0x2eD832Ba664535e5886b75D64C46EB9a228C2610)
@@ -48,11 +57,55 @@ struct RequestStatus {
         s_subscriptionId = subscriptionId;
     }
 
-    function changeGasLimit(uint32 _newGasLimit) external onlyContractOwner {
+    function changeGasLimit(uint32 _newGasLimit) external onlyOwner {
         callbackGasLimit = _newGasLimit;
     }
 
-    function _requestRandomWords(uint _gameNum) internal returns (uint256 requestId) {
+    function withdrawFees() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function newGame(string memory _HeadsOrTails) external payable {
+        require(msg.value >= 0.5 ether, "Too small of an amount.");
+        require(msg.value <= 10000 ether, "Too large of an amount.");
+        require(uint(keccak256(abi.encodePacked(_HeadsOrTails))) == uint(keccak256(abi.encodePacked("Heads"))) || uint(keccak256(abi.encodePacked("Tails"))) == uint(keccak256(abi.encodePacked(_HeadsOrTails))), "You must pick Heads or Tails.");
+        Games.push(Game(startingGame, msg.sender, _HeadsOrTails, msg.value, 0x0000000000000000000000000000000000000000, "", false, 0x0000000000000000000000000000000000000000, "", 0, false, new uint256[](0), 0));
+        startingGame.add(1);
+    }
+
+    function fillGame(uint32 _gameNum) public payable {
+        require(uint(msg.value) == Games[_gameNum].stake, "You must send the same amount of ETH as the other player.");
+        require(Games[_gameNum].filled == false, "This game has already been filled.");
+        Games[_gameNum].player2 = msg.sender;
+        Games[_gameNum].stake = Games[_gameNum].stake.mul(2);
+        if (uint(keccak256(abi.encodePacked(Games[_gameNum].player1Side))) == uint(keccak256(abi.encodePacked("Heads")))) {
+            Games[_gameNum].player2Side = "Tails";
+        } else {
+            Games[_gameNum].player2Side = "Heads";
+        }
+        Games[_gameNum].filled = true;
+        _requestRandomWords(_gameNum);
+    }
+
+    function flipGame(uint32 _gameNum) external {
+        require(s_requests[_gameNum].fulfilled = true);
+        s_requests[_gameNum].randomWords = Games[_gameNum].randomNum;
+        Games[_gameNum].roll = Games[_gameNum].randomNum[0].mod(100);
+         if (Games[_gameNum].roll >= 50) {
+            Games[_gameNum].winningSide = "Tails";
+        } else {
+            Games[_gameNum].winningSide = "Heads";
+        }
+        if ((uint(keccak256(abi.encodePacked(Games[_gameNum].winningSide))) == (uint(keccak256(abi.encodePacked(Games[_gameNum].player1Side)))))) {
+            payable(Games[_gameNum].player1).transfer(Games[_gameNum].stake.mul(98).div(100));
+            Games[_gameNum].winner = Games[_gameNum].player2;
+        } else {
+            payable(Games[_gameNum].player2).transfer(Games[_gameNum].stake.mul(98).div(100));
+            Games[_gameNum].winner = Games[_gameNum].player2;
+        }
+    }
+
+    function _requestRandomWords(uint32 _gameNum) internal returns (uint256 requestId) {
         requestId = COORDINATOR.requestRandomWords(
             keyHash,
             s_subscriptionId,
@@ -60,7 +113,7 @@ struct RequestStatus {
             callbackGasLimit,
             numWords
         );
-        s_requests[requestId] = RequestStatus({randomWords: new uint256[](0), exists: true, fulfilled: false, gameNum: _gameNum});
+        s_requests[requestId] = RequestStatus(false, true, new uint256[](0), _gameNum);
         Games[_gameNum].randomIdExists = true;
         Games[_gameNum].randomId = requestId;
         return requestId;
@@ -71,104 +124,4 @@ struct RequestStatus {
         s_requests[_requestId].fulfilled = true;
         s_requests[_requestId].randomWords = _randomWords;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-    function _requestRandomWords(uint _gameNum) internal returns (uint256 requestId) {
-        requestId = COORDINATOR.requestRandomWords(
-            keyHash,
-            s_subscriptionId,
-            requestConfirmations,
-            callbackGasLimit,
-            numWords
-        );
-        Requests.push(RequestStatus(false, true, new uint256[](0), _gameNum, requestId));
-        Games[_gameNum].randomId = requestId;
-        Games[_gameNum].randomIdExists = true;
-        return requestId;
-    }
-
-    function fulfillRandomWords(uint256 _requestId, uint256[] memory _randomWords) internal override {
-        Requests[_requestId].fulfilled = true;
-        Requests[_requestId].randomNum = _randomWords;
-    }
-*/
-    using SafeMath for uint;
-
-    uint startingGame = 0;
-
-    struct Game {
-        uint gameNum;
-        address player1;
-        string player1Side;
-        uint stake;
-        address player2;
-        string player2Side;
-        bool filled;
-        address winner;
-        uint amountWon;
-        string winningSide;
-        uint256 randomId;
-        bool randomFulfilled;
-        bool randomIdExists;
-        uint256[] randomNum;
-        uint roll;
-    }
-
-    Game[] public Games;
-
-    function withdrawFees() external onlyContractOwner {
-        payable(msg.sender).transfer(address(this).balance);
-    }
-
-    function newGame(string memory _HeadsOrTails) external payable {
-        require(msg.value >= 0.00001 ether, "Too small of an amount.");
-        require(uint(keccak256(abi.encodePacked(_HeadsOrTails))) == uint(keccak256(abi.encodePacked("Heads"))) || uint(keccak256(abi.encodePacked("Tails"))) == uint(keccak256(abi.encodePacked(_HeadsOrTails))), "You must pick Heads or Tails.");
-        startingGame = startingGame.add(1);
-        Games.push(Game(startingGame, msg.sender, _HeadsOrTails, msg.value, 0x0000000000000000000000000000000000000000, "", false, 0x0000000000000000000000000000000000000000, 0, "", 0, false, false, new uint256[](0), 0));
-    }
-
-    function fillGame(uint _gameNum) public payable {
-        require(uint(msg.value) == Games[_gameNum].stake, "You must send the same amount of ETH as the other player.");
-        require(Games[_gameNum].filled == false, "This game has already been filled.");
-        Games[_gameNum].player2 = msg.sender;
-        if (uint(keccak256(abi.encodePacked(Games[_gameNum].player1Side))) == uint(keccak256(abi.encodePacked("Heads")))) {
-            Games[_gameNum].player2Side = "Tails";
-        } else {
-            Games[_gameNum].player2Side = "Heads";
-        }
-        Games[_gameNum].filled = true;
-        _requestRandomWords(_gameNum);
-    }
-
-    function flipGame(uint _gameNum) external {
-        require(s_requests[_gameNum].fulfilled = true);
-        s_requests[_gameNum].randomWords = Games[_gameNum].randomNum;
-        Games[_gameNum].roll = Games[_gameNum].randomNum[0].mod(100);
-         if (Games[_gameNum].roll >= 50) {
-            Games[_gameNum].winningSide = "Tails";
-        } else {
-            Games[_gameNum].winningSide = "Heads";
-        }
-        if ((uint(keccak256(abi.encodePacked(Games[_gameNum].winningSide))) == (uint(keccak256(abi.encodePacked(Games[_gameNum].player1Side)))))) {
-            payable(Games[_gameNum].player1).transfer(Games[_gameNum].stake.mul(2).mul(98).div(100));
-            Games[_gameNum].winner = Games[_gameNum].player2;
-        } else {
-            payable(Games[_gameNum].player2).transfer(Games[_gameNum].stake.mul(2).mul(98).div(100));
-            Games[_gameNum].winner = Games[_gameNum].player2;
-        }
-        
-        Games[_gameNum].amountWon = Games[_gameNum].stake.mul(2).mul(98).div(100);
-    }
-
 } 
